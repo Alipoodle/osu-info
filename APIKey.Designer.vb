707da3b9bda103ff94a83eb653f4f9c5a1833e83
GreenTurtwig <greenturtwig@gmail.com>
2016-04-21 14:01:38 +0100

    Initial Upload

---
+++ APIKey.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class APIKey
+    Inherits MaterialSkin.Controls.MaterialForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.APIKeyBox = New MaterialSkin.Controls.MaterialSingleLineTextField()
+        Me.OK = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.SuspendLayout()
+        '
+        'APIKeyBox
+        '
+        Me.APIKeyBox.Depth = 0
+        Me.APIKeyBox.Hint = ""
+        Me.APIKeyBox.Location = New System.Drawing.Point(12, 71)
+        Me.APIKeyBox.MaxLength = 32767
+        Me.APIKeyBox.MouseState = MaterialSkin.MouseState.HOVER
+        Me.APIKeyBox.Name = "APIKeyBox"
+        Me.APIKeyBox.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.APIKeyBox.SelectedText = ""
+        Me.APIKeyBox.SelectionLength = 0
+        Me.APIKeyBox.SelectionStart = 0
+        Me.APIKeyBox.Size = New System.Drawing.Size(261, 23)
+        Me.APIKeyBox.TabIndex = 0
+        Me.APIKeyBox.TabStop = False
+        Me.APIKeyBox.UseSystemPasswordChar = False
+        '
+        'OK
+        '
+        Me.OK.Depth = 0
+        Me.OK.Location = New System.Drawing.Point(279, 71)
+        Me.OK.MouseState = MaterialSkin.MouseState.HOVER
+        Me.OK.Name = "OK"
+        Me.OK.Primary = True
+        Me.OK.Size = New System.Drawing.Size(49, 23)
+        Me.OK.TabIndex = 1
+        Me.OK.Text = "OK"
+        Me.OK.UseVisualStyleBackColor = True
+        '
+        'APIKey
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(340, 106)
+        Me.Controls.Add(Me.OK)
+        Me.Controls.Add(Me.APIKeyBox)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "APIKey"
+        Me.ShowIcon = False
+        Me.Sizable = False
+        Me.Text = "API Key"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents APIKeyBox As MaterialSkin.Controls.MaterialSingleLineTextField
+    Friend WithEvents OK As MaterialSkin.Controls.MaterialRaisedButton
+End Class
---
+++ Form1.vb
@@ -0,0 +1,120 @@
+﻿Imports MaterialSkin
+Imports osuElements
+Imports osuElements.Api.Repositories
+
+Public Class Form1
+    Dim uRep = New ApiUserRepository()
+    Dim bRep = New ApiBeatmapRepository()
+    Dim BMPs(4) As Integer
+    Dim GetBestCounter = 0
+
+
+    Public Async Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim SkinManager As MaterialSkinManager = MaterialSkinManager.Instance
+        SkinManager.AddFormToManage(Me)
+        If My.Settings.DarkTheme = "LIG0HT" Then
+            SkinManager.Theme = MaterialSkinManager.Themes.LIGHT
+        ElseIf My.Settings.DarkTheme = "DARK" Then
+            SkinManager.Theme = MaterialSkinManager.Themes.DARK
+        End If
+        SkinManager.ColorScheme = New ColorScheme(Primary.Pink600, Primary.Pink700, Primary.Pink300, Accent.LightBlue100, TextShade.WHITE)
+
+        If My.Settings.APIKey = Nothing Then
+            Dim Input = InputBox("Enter your API key", "osu!info")
+            Try
+                osuElements.osuElements.ApiKey = Input
+                Dim account = Await uRep.Get("GreenTurtwig")
+            Catch ex As Exception
+                Me.Hide()
+                MessageBox.Show("Invalid/no API key, please restart the program and enter again")
+                Application.Exit()
+            End Try
+            My.Settings.APIKey = Input
+        End If
+        osuElements.osuElements.ApiKey = My.Settings.APIKey
+
+        Best.Columns.Add("Rank")
+        Best.Columns.Add("Name", 220)
+        Best.Columns.Add("pp")
+    End Sub
+
+    Async Sub OK_Click(sender As Object, e As EventArgs) Handles OK.Click
+        Dim uRep = New ApiUserRepository()      'Remove these when the .ToString("N0") bug is fixed
+        Dim bRep = New ApiBeatmapRepository()
+        Best.Items.Clear()
+        GetBestCounter = 0
+
+        Dim account = Await uRep.Get(UsernameBox.Text)
+        AccountImage.ImageLocation = ("http://s.ppy.sh/a/" & account.UserId)
+        UsernameLabel.Text = account.Username
+        LevelLabel.Text = ("Level: " & Int(account.Level))
+        LevelProgress.Value = ((account.Level - Int(account.Level)) * 100)
+        RankedScore.Text = ("Ranked Score: " & account.RankedScore.ToString("N0"))
+        TotalScore.Text = ("Total Score: " & account.TotalScore.ToString("N0"))
+        Accuracy.Text = ("Accuracy: " & account.Accuracy & "%")
+        PlayCount.Text = ("Play Count: " & account.Playcount.ToString("N0"))
+        Performance.Text = ("Performance: " & Int(account.PpRaw.ToString("N0")) & "pp")
+
+        Rank.Text = ("#" & account.PpRank)
+        CountryRank.Text = (account.Country & " #" & account.PpCountryRank)
+
+        SSLabel.Text = account.SSCount
+        SLabel.Text = account.SCount
+        ALabel.Text = account.ACount
+
+        GetBest()
+    End Sub
+
+    Public Async Sub GetBest()
+        While GetBestCounter < 5
+            Dim account = Await uRep.Get(UsernameBox.Text)
+            Dim top = Await uRep.GetBest(account.UserId)
+
+            Dim one As New ListViewItem(ConvertRank(top.Item(GetBestCounter).Rank.ToString))
+            Dim map = Await bRep.Get(top.Item(GetBestCounter).BeatmapId)
+            one.SubItems.Add(map.Title)
+            one.SubItems.Add(Math.Round(top.Item(GetBestCounter).Pp))
+            BMPs(GetBestCounter) = map.BeatmapSetId
+            Best.Items.AddRange(New ListViewItem() {one})
+            GetBestCounter = GetBestCounter + 1
+        End While
+    End Sub
+
+    Public Function ConvertRank(ByVal rank As String) As String
+        If rank = "X" Then
+            rank = "SS"
+        ElseIf rank = "XH" Then
+            rank = "SS"
+        ElseIf rank = "SH" Then
+            rank = "S"
+        End If
+        ConvertRank = rank
+    End Function
+    Private Sub Best_ItemActivate(sender As Object, e As EventArgs) Handles Best.ItemActivate
+        osuURL.InClientDownload(BMPs(Best.FocusedItem.Index))
+    End Sub
+
+    Private Sub DarkThemeIcon_Click(sender As Object, e As EventArgs) Handles DarkThemeIcon.Click
+        If My.Settings.DarkTheme = "LIGHT" Then
+            SkinManager.Theme = MaterialSkinManager.Themes.DARK
+            My.Settings.DarkTheme = "DARK"
+        ElseIf My.Settings.DarkTheme = "DARK" Then
+            SkinManager.Theme = MaterialSkinManager.Themes.LIGHT
+            My.Settings.DarkTheme = "LIGHT"
+        End If
+    End Sub
+
+    Private Sub Best_ColumnWidthChanged(ByVal sender As Object, ByVal e As System.Windows.Forms.ColumnWidthChangedEventArgs) Handles Best.ColumnWidthChanged
+        Static FireMe As Boolean = True
+        If FireMe = True Then
+            FireMe = False
+            Best.Columns(0).Width = 60
+            Best.Columns(1).Width = 220
+            Best.Columns(2).Width = 60
+            FireMe = True
+        End If
+    End Sub
+    Private Sub Best_ColumnWidthChanging(ByVal sender As Object, ByVal e As System.Windows.Forms.ColumnWidthChangingEventArgs) Handles Best.ColumnWidthChanging
+        e.Cancel = True
+    End Sub
+End Class
